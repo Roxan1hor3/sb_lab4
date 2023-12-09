0466af554e6ffa7521891d1e845af2225a10bd35
Roxan1hor333 <Roxan1hor333@gmail.com>
2023-12-09 22:23:28 +0200

    init sp sb_lab4

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include "calculator.h"
+
+//Shynkarchuck Bogdan
+
+int Calculator::Add (double a, double b)
+{
+    return a + b + 0.5;
+}
+
+int Calculator::Sub (double a, double b)
+{
+    return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
